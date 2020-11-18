23283c7f1e6fb80709e7c2cf537d621010842bd5
Maggie Hibberd <maggiehibberd@gmail.com>
2020-11-18 15:00:57 +0000

    Added confirm email & password. Also 'presence' for name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
+  validates :name, presence: true
+  validates :email, confirmation: true, presence: true
+  validates :email_confirmation, presence: true
+  validates :password_digest, confirmation: true, presence: true
+  validates :password_digest_confirmation, presence: true
 end
