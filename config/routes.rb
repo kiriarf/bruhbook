b85b7d85e3eede12963fa4325341fe1cabea4042
Kiril <kiril@olgas-air.mynet>
2020-11-18 14:28:39 +0000

    add index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # resources :home
   resources :posts
   resources :users
+
+  get '/', to: 'application#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
