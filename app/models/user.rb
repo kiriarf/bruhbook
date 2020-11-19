d71f3364e9d76583faafa4c345eecafcad33c75e
Kiril <kiril@olgas-air.mynet>
2020-11-19 08:36:28 +0000

    add email uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :posts
   validates :name, presence: true
-  validates :email, confirmation: true, presence: true
+  validates :email, confirmation: true, presence: true, uniqueness: true
   validates :email_confirmation, presence: true
   validates :password, confirmation: true, presence: true
   validates :password_confirmation, presence: true
