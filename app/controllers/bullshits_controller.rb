2bcad4a76de2e7e61f1e1feed9a3cef7801cb8bd
Kiril <kiril@olgas-air.mynet>
2020-11-23 16:05:01 +0000

    1 like and bullhist per user

--- app/controllers/bullshits_controller.rb
+++ app/controllers/bullshits_controller.rb
@@ -2,12 +2,18 @@ class BullshitsController < ApplicationController
   before_action :find_post
 
   def create
-    @post.bullshits.create(user_id: current_user.id)
-    redirect_to request.referer + "#post-#{@post.id}"
+    if !already_bullshited?
+      @post.bullshits.create(user_id: current_user.id)
+      redirect_to request.referer + "#post-#{@post.id}"
+    end
   end
   
   private
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def already_bullshited?
+    Bullshit.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 end
