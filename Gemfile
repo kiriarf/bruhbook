77b177b14560269d4f5b159039e8930c265cff52
Kiril <kiril@olgas-air.mynet>
2020-11-24 17:06:45 +0000

    api works with localhost

--- Gemfile
+++ Gemfile
@@ -24,6 +24,12 @@ gem 'bcrypt', '~> 3.1.7'
 #travis for CI/CD
 gem 'travis'
 
+gem 'grape'
+
+gem 'rack-cors'
+
+gem 'grape-active_model_serializers'
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
+# Rails.application.routes.draw do
+#   root to: "application#index"
+#   resources :posts do
+#     resources :likes
+#     resources :bullshits
+#   end
+
+#   resources :users
+
+
+#   get '/signup', to: 'users#new'
+#   get '/login', to: 'sessions#login'
+#   post '/login', to: 'sessions#create'
+#   delete '/logout', to: 'sessions#destroy'
+
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
 Rails.application.routes.draw do
-  root to: "application#index"
-  resources :posts do
-    resources :likes
-    resources :bullshits
-  end
-
-  resources :users
-
-
-  get '/signup', to: 'users#new'
-  get '/login', to: 'sessions#login'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount API::Base, at: "/"
 end
