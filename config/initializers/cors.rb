df0061a1647966637d479a045db773ed9ddcf618
Kiril <kiril@olgas-air.mynet>
2020-11-24 16:44:33 +0000

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,9 +4,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
-  # allow do
-  #   origins "https://bruhbook.herokuapp.com"
-  #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  # end
+  allow do
+    origins "https://bruhbook-api.herokuapp.com"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
 end
